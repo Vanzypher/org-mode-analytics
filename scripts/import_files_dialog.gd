af944d321a5afe67de2a8c3f1e065a66b277c476
Vanzypher <vanzypher@gmail.com>
2019-05-29 07:00:26 -0300

    get the contents of the file

--- scripts/import_files_dialog.gd
+++ scripts/import_files_dialog.gd
@@ -9,8 +9,16 @@ func _on_ImportFilesDialog_files_selected(paths: PoolStringArray):
 	for path in paths:
 		var file_info: Dictionary = {}
 		var file_name = path.get_file()
+		
+		var file = File.new()
+		file.open(path,File.READ)
+		var content = file.get_as_text()
+		file.close()
+		
 		file_info["file_name"] = file_name
 		file_info["file_path"] = path
+		file_info["file_contents"] = content
+		
 		item_list.add_item(file_name)
 		emit_signal("file_added", file_info)
 
